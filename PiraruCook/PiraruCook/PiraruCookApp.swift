2f840a4d039cdaf1ec6ba9b215c7c194d01b0c74
Pamella <pamella.as.304@gmail.com>
2024-04-24 08:25:15 -0300

    created file

---
+++ PiraruCook/PiraruCook/PiraruCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiraruCookApp.swift
+//  PiraruCook
+//
+//  Created by Pamella Alvarenga on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PiraruCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

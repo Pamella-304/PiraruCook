7cdf531e13a7c40e710c6f25ef5d4a5d55e6f7a4
Pamella <pamella.as.304@gmail.com>
2024-05-02 10:04:57 -0300

    sign in button implemented

--- PiraruCook/PiraruCook/Views/ProfileView.swift
+++ PiraruCook/PiraruCook/Views/ProfileView.swift
@@ -4,15 +4,75 @@
 //
 //  Created by Pamella Alvarenga on 02/05/24.
 //
-
+import AuthenticationServices
 import SwiftUI
 
 struct ProfileView: View {
     
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userID") var userID: String = ""
+
+    private var isSignedIn: Bool {
+        !userID.isEmpty
+    }
+    
+    var body: some View {
+        VStack{
+            
+            if !isSignedIn {
+                ProfileViewModel()
+            }
+            
+           
+            
+        }.navigationTitle("Fazer login")
+    }
+        
+        
+}
+
+struct ProfileViewModel: View {
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userID") var userID: String = ""
     
     var body: some View {
         
-        Text("User Profile")
+        SignInWithAppleButton(.signIn) { request in
+            
+            request.requestedScopes = [.email, .fullName]
+            
+        } onCompletion: { result in
+            
+            switch result {
+            case .success(let auth):
+                switch auth.credential {
+                case let credential as ASAuthorizationAppleIDCredential:
+                    
+                    let userID = credential.user
+                    
+                    let email = credential.email
+                    let firstName = credential.fullName?.givenName
+                    let lastName = credential.fullName?.familyName
+                    
+                    self.userID = userID
+                    self.email = email ?? ""
+                    self.firstName = firstName ?? ""
+                    self.lastName = lastName ?? ""
+                    
+                default:
+                    break
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }.frame(height: 50)
+            .padding()
+            .cornerRadius(8)
     }
 }
 

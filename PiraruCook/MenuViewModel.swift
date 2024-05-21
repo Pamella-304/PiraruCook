a67c8d4ee8ef3a60face6f30d9bf63e9657822ee
Gabriel de Freitas Leite <gabriel.dfleite@gmail.com>
2024-05-21 08:47:17 -0300

    menu to sheet

--- PiraruCook/MenuViewModel.swift
+++ PiraruCook/MenuViewModel.swift
@@ -7,24 +7,25 @@
 
 import Foundation
 
-extension MenuView {
+
+@Observable
+class MenuViewModel {
     
-    @Observable
-    class MenuViewModel {
+    var dishesList = TypeDishesListViewModel()
+    
+    var categories: [String] {
+        var vetor = [String]()
         
-        var dishesList = TypeDishesListViewModel()
-        
-        var categories: [String] {
-            var vetor = [String]()
-            
-            for dish in dishesList {
-                if !vetor.contains(dish.tipo){
-                    vetor.append(dish.tipo)
-                }
+        for dish in dishesList {
+            if !vetor.contains(dish.tipo){
+                vetor.append(dish.tipo)
             }
-            
-            return vetor.sorted()
         }
+        
+        return vetor.sorted()
     }
     
+    var isPresented = false
+    var choosenDish: TypeDish?
 }
+

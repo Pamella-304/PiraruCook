929b3c734ce810c12f996ecb0c137b08b20bcd4d
Silvana Alves <silvanarodriguesalvess@gmail.com>
2024-05-23 09:34:48 -0300

    ajustando metodo de pagamento

---
+++ PiraruCook/PiraruCook/Views/TrackOrderView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackOrderView.swift
+//  PiraruCook
+//
+//  Created by Silvana Rodrigues Alves on 23/05/24.
+//
+
+import SwiftUI
+
+struct TrackOrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackOrderView()
+}

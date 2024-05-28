87433c1d9b418e207b21ca84b958b5bbb017dc32
Gabriel de Freitas Leite <gabriel.dfleite@gmail.com>
2024-05-28 09:12:17 -0300

    formatacao

--- PiraruCook/PiraruCook/Views/PaymentView.swift
+++ PiraruCook/PiraruCook/Views/PaymentView.swift
@@ -3,31 +3,31 @@
 import SwiftUI
 
 struct PaymentView: View {
-
+    
     @State var paymentSubtitle: String = ""
     @Environment(CartViewModel.self) private var viewModel
     @State private var finalPayment = false
-
+    
     var body: some View {
         //MARK: Payment Method
         VStack{
             HStack{
                 Text("Método de pagamento")
                     .font(Font(Fonts.title3Font))
-
+                
                 Spacer()
             }.padding(.horizontal)
             
             PaymentSections(outImage: true, usage: "Método de pagamento", img1: "Pix", subtext: paymentSubtitle, text: viewModel.paymentMethod.rawValue)
                 .font(Font(Fonts.title3Font))
-
+            
             HStack{
                 Spacer()
             }.padding(.horizontal)
             PaymentSections(outImage: false, usage: "Cupom", img1: "ticket.fill", subtext: "\(2) cupons disponíveis", text: "Cupom")
                 .font(Font(Fonts.title3Font))
-
-                
+            
+            
             HStack{
                 Text("Entrega")
                     .font(Font(Fonts.title3Font))
@@ -35,27 +35,27 @@ struct PaymentView: View {
             }.padding(.horizontal)
             PaymentSections(outImage: false, usage: "", img1: "mappin", subtext: "Entrega em até 40 min", text: "R. Silvio da Silva")
                 .font(Font(Fonts.title3Font))
-
-                
+            
+            
             Spacer()
             HStack{
                 Text("Resumo da Compra").bold()
-                .font(Font(Fonts.title3Font))
-
+                    .font(Font(Fonts.title3Font))
+                
                 Spacer()
             }.padding()
             ValuesSection(Title: "Subtotal", Price: viewModel.subTotalValue)
-                
+            
             ValuesSection(Title: "Desconto", Price: viewModel.getDiscount())
-                
-
+            
+            
             ValuesSection(Title: "Entrega", Price: viewModel.transportationValue)
-                
-
+            
+            
             ValuesSection(Title: "Total", Price: viewModel.getTotalValue())
                 .fontWeight(.semibold)
-
-
+            
+            
             
             
             Button{
@@ -66,17 +66,20 @@ struct PaymentView: View {
                     .foregroundColor(.white)
                     .padding(16)
                     .frame(maxWidth: .infinity)
-
+                
                     .background(.brandPrimary)
                     .font(Font(Fonts.title3Font))
-
-
+                
+                
             }
             .cornerRadius(10)
             .padding(16)
             
-        
-        }.navigationTitle("Pagamento").sheet(isPresented: $finalPayment) {
+            
+        }
+        .navigationTitle("Pagamento")
+        .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $finalPayment) {
             ConfirmOrderView().presentationDetents((viewModel.paymentMethod.rawValue == "Dinheiro") ? [.fraction(0.45)] : [.fraction(0.3)])
         }
         
